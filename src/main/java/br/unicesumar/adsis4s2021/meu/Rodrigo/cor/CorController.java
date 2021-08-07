8fb4d9ef2b9c0cf7a456a642668fdae13c5c64cb
Rodrigobueno2 <73552177+Rodrigobueno2@users.noreply.github.com>
2021-08-07 01:46:07 -0300

    Adicionando put post delete e get por sigla das cores

--- src/main/java/br/unicesumar/adsis4s2021/meu/Rodrigo/cor/CorController.java
+++ src/main/java/br/unicesumar/adsis4s2021/meu/Rodrigo/cor/CorController.java
@@ -3,8 +3,11 @@ package br.unicesumar.adsis4s2021.meu.Rodrigo.cor;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,11 +30,48 @@ public class CorController {
 		return this.cores;
 	}
     
+    @GetMapping("/{sigla}")
+    public Cor getCorespelasigla(@PathVariable("sigla") String sigla) {
+		for (Cor c: cores) {
+			if(c.getSigla().equalsIgnoreCase(sigla)) {
+				return c;
+			}
+			
+		}
+		
+		return null;
+	}
+    
     
     //curl -d '{"nome":"TESTE","sigla":"TST"}' -H "Content-Type: application/json" -X POST localhost:9095/cores
     @PostMapping
     public void postCor(@RequestBody Cor nova) {
     	this.cores.add(nova);
     }
+    
+    @DeleteMapping("/{sigla}")
+    public void deleteCorPelaSigla(@PathVariable("sigla") String sigla) {
+       Cor corParaRemover = null;
+       for(Cor c : cores) {
+    	   if(c.getSigla().equalsIgnoreCase(sigla)) {
+				corParaRemover = c;
+				break;
+			}
+       }
+       if(corParaRemover != null) {
+    	   cores.remove(corParaRemover);
+       }
+    	
+    }
+    
+    @PutMapping("/{sigla}")
+	public void updateCorPelaSigla(
+			@PathVariable("sigla") String sigla,
+			@RequestBody Cor corAtualizada) {
+
+		deleteCorPelaSigla(sigla);
+		this.cores.add(corAtualizada);
+
+	}
 	
 }
